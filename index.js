fa1d42a72ebde4650a7663463c02ad474afb1b08
clint360 <clint360.rebase@gmail.com>
2023-07-26 16:48:16 +0100

    started creating routes

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/status', (req, res)=>{
+    const status = {
+        Status: 'Running'
+     };
+     console.log(req);
+     res.send(status); 
+});
+
+app.listen(3000, () => {
+    console.log(`Server Started at ${3000}`)
+})
